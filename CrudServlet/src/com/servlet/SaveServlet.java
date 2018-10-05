357afdeb166e04f121348ba548b44409e9707019
chaman bharti <chaman.bharti84@gmail.com>
2018-10-05 21:28:50 +0530

    commented on condition loop

--- CrudServlet/src/com/servlet/SaveServlet.java
+++ CrudServlet/src/com/servlet/SaveServlet.java
@@ -50,7 +50,7 @@ public class SaveServlet extends HttpServlet{
 		         ps.setString(6,userId);  
 		         
 		         int i=ps.executeUpdate();
-		         if(i>0)  {
+		         if(i>0)  {//this is condition
 						RequestDispatcher rd=req.getRequestDispatcher("login.jsp");
 						rd.forward(req, resp);
 					}else{
