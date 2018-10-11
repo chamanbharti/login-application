61ebfdaef547bfe3b79dc6385150b39b651b8577
Chaman Bharti <chaman.bharti84@gmail.com>
2018-10-11 21:04:26 +0530

    Update SaveServlet.java

--- CrudServlet/src/com/servlet/SaveServlet.java
+++ CrudServlet/src/com/servlet/SaveServlet.java
@@ -29,7 +29,7 @@ public class SaveServlet extends HttpServlet{
 		}
 		String interest = req.getParameter("interest");
 		
-		//User user=null; first commit
+		//User user=null; first commit from command prompt
 		Connection con=null;
 		PreparedStatement ps = null;
 		ResultSet rs=null;
